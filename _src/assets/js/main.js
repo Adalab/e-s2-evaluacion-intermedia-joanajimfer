966d46e511cbc4582e4637c2d589b90131c27f6c
joanajimfer <joanajimfer@gmail.com>
2018-12-07 16:56:28 +0100

    Fixing labels and indent

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function getRandomNumber(max) {
-    const randomNumber = Math.ceil(Math.random() * max);
-    return randomNumber;
-  }
+  const randomNumber = Math.ceil(Math.random() * max);
+  return randomNumber;
+}
 const myRandomNumber = getRandomNumber(100);
 console.log(myRandomNumber);
-  
+
 
 const tryBtn = document.querySelector('.btn__number');
 
@@ -14,22 +14,17 @@ const playerNumber = document.querySelector('.box__number');
 const writtingPlace = document.querySelector('.answer__number');
 let counter = document.querySelector('.play__counter');
 
-function showPlayerNumber () {
-    const writePlayerNumber = playerNumber.value;
-    if (parseInt(writePlayerNumber) === myRandomNumber) {
-        writtingPlace.innerHTML = '¡HAS GANADO, CAMPEONA!';
-    } else if (parseInt(writePlayerNumber) > myRandomNumber) {
-        writtingPlace.innerHTML = 'Demasiado alto';
-    } else if (parseInt(writePlayerNumber) < myRandomNumber) {
-        writtingPlace.innerHTML = 'Demasiado bajo';
-    };
-    counter.innerHTML = parseInt(counter.innerHTML) + 1;
-    return console.log(writePlayerNumber);
-    }
+function showPlayerNumber() {
+  const writePlayerNumber = playerNumber.value;
+  if (parseInt(writePlayerNumber) === myRandomNumber) {
+    writtingPlace.innerHTML = '¡HAS GANADO, CAMPEONA!';
+  } else if (parseInt(writePlayerNumber) > myRandomNumber) {
+    writtingPlace.innerHTML = 'Demasiado alto';
+  } else if (parseInt(writePlayerNumber) < myRandomNumber) {
+    writtingPlace.innerHTML = 'Demasiado bajo';
+  };
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+  return console.log(writePlayerNumber);
+}
 
 tryBtn.addEventListener('click', showPlayerNumber);
-
-
-
-
-
