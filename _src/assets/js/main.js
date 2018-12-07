e49c55a372941ebac8e0f4a7bc4f387e266bfb77
joanajimfer <joanajimfer@gmail.com>
2018-12-07 15:55:00 +0100

    Working js added

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/* Primer paso, conseguimos un número aleatorio entre 0 y 100, que se presentará en la consola cada vez que se recargue la página
-*/
-
-// Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
     const randomNumber = Math.ceil(Math.random() * max);
     return randomNumber;
@@ -12,37 +8,26 @@ const myRandomNumber = getRandomNumber(100);
 console.log(myRandomNumber);
   
 
-/* Segundo paso, al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
-*/
-
 const tryBtn = document.querySelector('.btn__number');
 const playerNumber = document.querySelector('.box__number');
+const writtingPlace = document.querySelector('.answer__number');
+let counter = document.querySelector('.play__counter');
 
 function showPlayerNumber () {
-    const PlayerNumber = playerNumber.value;
-    console.log(PlayerNumber);
-    return PlayerNumber;
-} 
-const myPlayerNumber = showPlayerNumber();
-
+    const writePlayerNumber = playerNumber.value;
+    if (parseInt(writePlayerNumber) === myRandomNumber) {
+        writtingPlace.innerHTML = 'Lo has conseguido';
+    } else if (parseInt(writePlayerNumber) > myRandomNumber) {
+        writtingPlace.innerHTML = 'Es un número muy alto';
+    } else if (parseInt(writePlayerNumber) < myRandomNumber) {
+        writtingPlace.innerHTML = 'Es un número muy bajo';
+    };
+    counter.innerHTML = parseInt(counter.innerHTML) + 1;
+    return console.log(writePlayerNumber);
+    }
 
 tryBtn.addEventListener('click', showPlayerNumber);
 
-/* Tercer paso, comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla
-*/
-const textbox = document.querySelector('.answer__number');
-
-/*
-if (myRandomNumber === myPlayerNumber) {
-    textbox.innerHTML = "HAS GANADO";
-} else if (myPlayerNumber > myRandomNumber) {
-    textbox.innerHTML = "Demasiado alto";
-} else if (myPlayerNumber < myRandomNumber) {
-    textbox.innerHTML = "Demasiado bajo";
-} else {
-    textbox.innerHTML = textbox.innerHTML;
-}
-*/
 
 
 
